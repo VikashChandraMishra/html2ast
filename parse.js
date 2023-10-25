37a2e94035ceed42bffecaa1a1d2a80b3af92dea
VikashChandraMishra <crimsonsyrus000@gmail.com>
2023-10-25 12:11:15 +0530

    fixed node mismatch bug

--- parse.js
+++ parse.js
@@ -8,30 +8,30 @@ const endTagRegex = /(<\/)\w+(>)/i;
 
 const root = new AST().root;
 var cur = root;
-var temp;
 var contentFlag = false;
 
 const parse = (html, currentIndex) => {
 
-    var index = currentIndex;
+    for (let i = currentIndex; i < html.length; i++) {
 
-    for (let i = index; i < html.length; i++) {
+        // push content
+        if (contentFlag && html[i] !== '<') {
 
-        if (contentFlag) {
-
+            contentStack.push(html[i]);
             if (html[i + 1] === '<') {
                 contentFlag = false;
                 cur.data.content += contentStack.items.join('');
                 contentStack.clear();
             }
-            contentStack.push(html[i]);
 
         } else {
 
             tagStack.push(html[i]);
+
+            // deal with start tag encounter
             if (tagStack.items.join('').search(startTagRegex) !== -1) {
                 childIndex = cur.addChild({ tag: tagStack.items.join(''), content: '' });
-                temp = cur;
+                let temp = cur;
                 cur = cur.children[childIndex];
                 tagStack.clear();
                 contentFlag = true;
@@ -40,22 +40,21 @@ const parse = (html, currentIndex) => {
 
             }
 
+            // deal with end tag encounter
             if (tagStack.items.join('').search(endTagRegex) !== -1) {
                 tagStack.clear();
                 contentFlag = true;
-                index = i;
-                break;
+                return (i + 1);
             }
         }
 
     }
 
-    return (index + 1);
 }
 
 const parseWrap = (data, currentIndex) => {
     parse(data, currentIndex);
-    console.log(root.children[0].children[0].children)
+    console.log(root.children[0].children)
 }
 
 module.exports = { parse, parseWrap };
